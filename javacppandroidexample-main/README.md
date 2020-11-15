04bf320eecfa69dbf2165642ca579529426a63c6
jacobkrieg10 <74514643+jacobkrieg10@users.noreply.github.com>
2020-11-16 01:19:25 +0200

    Add files via upload

---
+++ javacppandroidexample-main/README.md
@@ -0,0 +1,3 @@
+# JavaCpp Android Example
+
+Simple Android app that prints on the screen a widget containing text received from C++.
---
+++ javacppandroidexample-main/javacpp/app/src/main/java/com/example/javacppapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.javacppapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Pointer objects allocated in Java get deallocated once they become unreachable,
+        // but C++ destructors can still be called in a timely fashion with Pointer.deallocate()
+        NativeLibrary.NativeClass l = new NativeLibrary.NativeClass();
+        l.set_property("Hello World!");
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
